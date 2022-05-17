d299fd379b15b6f2c38402e6f3a07cee4e3f0999
hlchanad <hlchanad@chanhonlun.com>
2022-05-17 10:44:31 +0800

    integrate jest

---
+++ jest.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    moduleFileExtensions: ['js', 'json'],
+    rootDir: 'src',
+    testRegex: '.*\\.*spec\\.js$',
+    collectCoverageFrom: ['**/*.js'],
+    coverageDirectory: '../coverage',
+    testEnvironment: 'node',
+};
